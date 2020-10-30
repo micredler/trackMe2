093dd03d26d6dd3b28ae4ffcccde5c5aad4808f9
UgoBk <73038880+UgoBk@users.noreply.github.com>
2020-10-30 11:57:33 +0200

    add internet support + fix on TA

--- Map.cs
+++ Map.cs
@@ -20,7 +20,7 @@ namespace trackMe
             WebView webView = FindViewById<WebView>(Resource.Id.webView);
             webView.SetWebViewClient(new WebViewClient());
             webView.Settings.JavaScriptEnabled = true;
-            webView.LoadUrl("https://www.govmap.gov.il/map.html?b=0&c=210555.53,648245.92&z=1&lay=TRAIN_STATOINS,BUS_STOPS");
+            webView.LoadUrl("https://www.govmap.gov.il/map.html?b=0&c=180611.64,663993.55&z=4&lay=TRAIN_STATOINS,BUS_STOPS");
         }
     }
 }
