6c4f0313727ba58a7ac093f9e8b654dcd5438444
Your NameMicha <micha@eprsys.co.il>
2020-11-06 12:05:31 +0200

    search by station work

---
+++ BL/ApiService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Java.Util.Functions;
+using Org.Json;
+
+namespace trackMe.BL
+{
+    class ApiService
+    {
+        const string BASE_URL = "http://www.ravgioraredler.co.il/api/tracks/getTracksData?url=";
+
+
+        public async Task<ApiResponse> GetDataFromApi(string urlParam)
+        {
+            string apiUrl = BASE_URL + urlParam;
+            string data = "";
+
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(apiUrl);
+                client.DefaultRequestHeaders.Accept.Clear();
+
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+                if (response.IsSuccessStatusCode)
+                {
+                    data = await response.Content.ReadAsStringAsync();
+                    data = data.Replace("-version", "version");
+                    ApiResponse table = Newtonsoft.Json.JsonConvert.DeserializeObject<ApiResponse>(data);
+
+                    return table;
+                }
+
+            }
+            return null;
+
+        }
+
+    }
+}
--- SrcByStation.cs
+++ SrcByStation.cs
@@ -1,14 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
-
+using System.Threading.Tasks;
 using Android.App;
 using Android.Content;
 using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using trackMe.BL;
 
 namespace trackMe
 {
@@ -20,6 +22,60 @@ namespace trackMe
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.src_by_station);
             // Create your application here
+            TextView txtStation = FindViewById<TextView>(Resource.Id.txt_staion_num);
+            Button btnSearch = FindViewById<Button>(Resource.Id.btn_search_station);
+            TableLayout mTableLayout = FindViewById<TableLayout>(Resource.Id.table_by_station);
+
+            btnSearch.Click += delegate
+            {
+                GetData(txtStation.Text, mTableLayout);
+
+            };
+
+        }
+
+        public async void GetData(string stationNum, TableLayout mTableLayout)
+        {
+            const string STATION_PARAM = "MonitoringRef=";
+            ApiService apiService = new ApiService();
+            ApiResponse j = await apiService.GetDataFromApi(STATION_PARAM + stationNum);
+            List<MonitoredStopVisit> visits = j.Siri.ServiceDelivery.StopMonitoringDelivery[0].MonitoredStopVisit.ToList();
+            setTableData(visits, mTableLayout);
+            //System.Diagnostics.Debug.WriteLine(j);
+        }
+
+        public void setTableData(List<MonitoredStopVisit> data, TableLayout mTableLayout)
+        {
+            while (mTableLayout.ChildCount > 1)
+            {
+                mTableLayout.RemoveViewAt(1);
+            }
+            TableRow.LayoutParams tableRowLayoutparams = new TableRow.LayoutParams(
+                ViewGroup.LayoutParams.WrapContent,
+                ViewGroup.LayoutParams.WrapContent);
+
+            foreach (MonitoredStopVisit Row in data)
+            {
+                TableRow tr = new TableRow(this);
+
+                tr.LayoutParameters = tableRowLayoutparams;
+                TextView tv1 = new TextView(this);
+                tv1.Text = Row.MonitoredVehicleJourney.PublishedLineName;
+                tv1.TextAlignment = TextAlignment.ViewEnd;
+
+                TextView tv2 = new TextView(this);
+                tv2.Text = Row.MonitoredVehicleJourney.MonitoredCall.ExpectedArrivalTime.ToShortTimeString();
+                tv2.TextAlignment = TextAlignment.ViewEnd;
+
+                TextView tv3 = new TextView(this);
+                tv3.Text = Row.MonitoredVehicleJourney.MonitoredCall.DistanceFromStop;
+                tv3.TextAlignment = TextAlignment.ViewEnd;
+                tr.AddView(tv3);
+                tr.AddView(tv2);
+                tr.AddView(tv1);
+                mTableLayout.AddView(tr);
+
+            }
         }
     }
 }
