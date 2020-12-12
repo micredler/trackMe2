5ad8b23cdadd4583992209576bc8661fec266dcf
UgoBk <73038880+UgoBk@users.noreply.github.com>
2020-12-13 00:37:32 +0200

    add-destiantion in db + list of destination in srcByNum

--- Data/Route.cs
+++ Data/Route.cs
@@ -7,8 +7,8 @@ namespace trackMe.Data
         [PrimaryKey, AutoIncrement]
         public int route_id { get; set; }
         public int agency_id { get; set; }
-        public string route_short_name { get; set; }
-        public string route_long_name { get; set; }
+        public string start_from { get; set; }
+        public string destination { get; set; }
         public string route_desc { get; set; }
         public int route_type { get; set; }
         public string route_color { get; set; }
