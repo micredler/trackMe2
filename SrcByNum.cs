981f8e354ed95b67f485406c8f256b1f25b264c7
UgoBk <73038880+UgoBk@users.noreply.github.com>
2020-12-13 21:19:55 +0200

    use selected destination and use it in API

--- SrcByNum.cs
+++ SrcByNum.cs
@@ -31,21 +31,27 @@ namespace trackMe
 
             operatorAutoComplete.Adapter = adapter;
 
-            List<string> directions = GetDirections(txtLine.Text, operatorAutoComplete.Text).Select(direction => direction.destination).ToList();
+            List<Route> directions = GetDirections(txtLine.Text, operatorAutoComplete.Text);
 
+            List<string> optionalDirections = directions.Select(direction => direction.destination).ToList();
+
+            // here you'll use the direction
+            // and choose string from optionalDirections
+            var fakeDirectionThatChoosen = "קו לתל אביב";
+
+            int routeIdOfDirectionChoosen = directions.First(d => d.destination.Equals(fakeDirectionThatChoosen)).route_id;
 
             btnSearch.Click += delegate
             {
                 labelFavorite.Visibility = Android.Views.ViewStates.Invisible;
                 labelFavorite.Text = "";
-                GetData(txtLine.Text, mTableLayout, operatorAutoComplete.Text);
-
+                GetData(txtLine.Text, mTableLayout, operatorAutoComplete.Text, routeIdOfDirectionChoosen);
             };
 
             btnFavorite.Click += delegate
             {
                 string favoriteName = "חברה " + operatorAutoComplete.Text + " קו " + txtLine.Text;
-                dbHelper.AddNewFavorite(this, favoriteName, GetSrcUrl(txtLine.Text, operatorAutoComplete.Text), (int)SEARCH_TYPE.line);
+                dbHelper.AddNewFavorite(this, favoriteName, GetSrcUrl(routeIdOfDirectionChoosen), (int)SEARCH_TYPE.line);
                 Alert.AlertMessage(this, "הודעת מערכת", favoriteName + " נוסף למועדפים");
                 //Alert("the url is", GetSrcUrl(txtLine.Text, operatorAutoComplete.Text));
             };
@@ -57,22 +63,18 @@ namespace trackMe
                 labelFavorite.Visibility = Android.Views.ViewStates.Visible;
                 string searchName = Intent.GetStringExtra("searchName");
                 labelFavorite.Text = searchName;
-                GetData("", mTableLayout, "", favoriteUrl);
+                GetData("", mTableLayout, "", routeIdOfDirectionChoosen, favoriteUrl);
             }
-
         }
 
-
-        public string GetSrcUrl(string lineNumFromUser, string agencySelected)
+        public string GetSrcUrl(int routeIdOfDirectionChoosen)
         {
             const string AND_SIGN = "%26";
             const string STATION_PARAM = "MonitoringRef=all";
             const string LINE_PARAM = "LineRef=";
             const string CALLS = "StopVisitDetailLevel=calls";
 
-            string routeId = dbHelper.GetRouteIdFromDB(lineNumFromUser, agencySelected);
-
-            if (String.IsNullOrEmpty(routeId))
+            if (routeIdOfDirectionChoosen == 0)
             {
                 Alert.AlertMessage(this, "הודעת מערכת", "מספר הקו לא מופיע במערכת");
                 return null;
@@ -80,14 +82,14 @@ namespace trackMe
 
             else
             {
-                return STATION_PARAM + AND_SIGN + LINE_PARAM + routeId + AND_SIGN + CALLS;
+                return STATION_PARAM + AND_SIGN + LINE_PARAM + routeIdOfDirectionChoosen + AND_SIGN + CALLS;
             }
         }
 
-        public async void GetData(string lineNumFromUser, TableLayout mTableLayout, string agencySelected, string favoriteUrl = "")
+        public async void GetData(string lineNumFromUser, TableLayout mTableLayout, string agencySelected, int routeIdOfDirectionChoosen, string favoriteUrl = "")
         {
             ApiService apiService = new ApiService();
-            string urlToSend = favoriteUrl != "" ? favoriteUrl : GetSrcUrl(lineNumFromUser, agencySelected);
+            string urlToSend = favoriteUrl != "" ? favoriteUrl : GetSrcUrl(routeIdOfDirectionChoosen);
             ApiResponse apiResponse = await apiService.GetDataFromApi(urlToSend);
 
             if (apiResponse.Siri != null)
